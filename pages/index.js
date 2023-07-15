a1e50bd79885a0346d8f04c4ee7473184104322c
Kevin Quispe <kevincarlosqa@gmail.com>
2023-07-15 16:24:48 -0500

    add: Setup Project

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Head from "next/head";
+import { PostCard, Categories, PostWidget } from "@/components";
 
 const posts = [
   { title: "React Testing", excerpt: "Learn React Testing" },
@@ -11,18 +12,23 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-10 mb-8">
+    <div className="container mx-auto px-10 mb-8 bg-gray-300">
       <Head>
         <title>Kevin Blog</title>
         <link />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        {posts.map((post, index) => {
-          <div>
-            {post.title}
-            {post.excerpt}
-          </div>;
-        })}
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard post={post} key={post.title} />
+          ))}
+        </div>
+      </div>
+      <div className="lg:col-span-4 col-span-1">
+        <div className="lg:sticky relative top-8">
+          <PostWidget />
+          <Categories />
+        </div>
       </div>
     </div>
   );
