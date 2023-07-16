de03eaa62600dee11e8a4c9b6bf120557a91b50d
Kevin Quispe <kevincarlosqa@gmail.com>
2023-07-16 00:13:13 -0500

    chore: PostWidget info

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import moment from "moment";
 import Link from "next/link";
 import { getRecentPosts, getSimilarPosts } from "@/services";
+import Image from "next/image";
 
 const PostWidget = ({ categories, slug }) => {
   const [relatedPosts, setRelatedPosts] = useState([]);
@@ -17,7 +18,34 @@ const PostWidget = ({ categories, slug }) => {
   }, [slug]);
 
   console.log(relatedPosts);
-  return <div>PostWidget</div>;
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+      <h3 className="tex-xl mb-8 font-semibold border-b pb-4">
+        {slug ? "Related Posts" : "Recent Posts"}
+      </h3>
+      {relatedPosts.map((post, index) => (
+        <div key={post.title} className="flex items-center w-full mb-4">
+          <div className="w-16 flex-none">
+            <Image
+              src={post.featuredImage.url}
+              alt={post.title}
+              height={60}
+              width={60}
+              className="align-middle rounded-full"
+            />
+          </div>
+          <div className="flex-row ml-4">
+            <p className="text-gray-500 font-xs">
+              {moment(post.createdAt).format("MMM DD, YYYY")}
+            </p>
+            <Link href={`/post/${post.slug}`} className="text-md">
+              {post.title}
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default PostWidget;
